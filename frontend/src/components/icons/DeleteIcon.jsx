90565a6075db8d8c2f87b7d3b5e4071c045770d4
AridaneTheWiseDream <aridane@internship.thewisedreams.com>
2024-03-19 16:06:10 +0000

    change the icons

--- frontend/src/components/icons/DeleteIcon.jsx
+++ frontend/src/components/icons/DeleteIcon.jsx
@@ -2,10 +2,11 @@ export const DeleteIcon = (style) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512"
-      className={` ${style.className}`}
+      viewBox="0 0 20 20"
+      className={`${style.className}`}
+      fill="currentColor"
     >
-      <path d="M135.2 17.7 128 32H32C14.3 32 0 46.3 0 64s14.3 32 32 32h384c17.7 0 32-14.3 32-32s-14.3-32-32-32h-96l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32l21.2 339c1.6 25.3 22.6 45 47.9 45h245.8c25.3 0 46.3-19.7 47.9-45L416 128z" />
+      <path d="M15 4a1 1 0 0 0-1-1h-3V2H9v1H6a1 1 0 0 0-1 1v1H3v2h1v9a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V8h1V6h-1V5a1 1 0 0 0-1-1h-3zm-4 1h2v1h-2V5zM8 5h2v1H8V5zM6 8h8v9H6V8z" />
     </svg>
   );
 };
