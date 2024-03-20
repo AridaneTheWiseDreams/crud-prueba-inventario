1ad15d3157b971fc14b760ccf06d2ded6b03e310
AridaneTheWiseDream <aridane@internship.thewisedreams.com>
2024-03-20 09:20:27 +0000

    added route in app config

--- backend/src/config/app.config.js
+++ backend/src/config/app.config.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import productsRoute from "../routes/products.routes.js";
+import usersRoute from "../routes/users.routes.js";
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.use("/api/products", productsRoute);
+app.use("/api/users", usersRoute);
 
 export default app;
