784bc65733f2fa69a9cb1e62ce0fc8969054b0bd
AridaneTheWiseDream <aridane@internship.thewisedreams.com>
2024-03-20 09:19:00 +0000

    more icons

---
+++ frontend/src/components/icons/SearchIcon.jsx
@@ -0,0 +1,18 @@
+export const SearchIcon = (style) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      className={`${style.className}`}
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m21 21-5.2-5.2m2.8 5.2c-2.5-2.5-5.5-4-8.7-4.2C6.4 17 3 13.6 3 9.5S6.4 2 10.5 2c4.2 0 7.7 3.4 7.7 7.5 0 3.2-1.5 6.2-4 8.2z"
+      />
+    </svg>
+  );
+};
