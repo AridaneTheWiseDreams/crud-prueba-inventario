1877a94903de92d0a5c279e04327ee458fcf3f44
AridaneTheWiseDream <aridane@internship.thewisedreams.com>
2024-03-21 15:06:32 +0000

    added a new backend endpoint

--- frontend/src/constants/backendEdpoint.js
+++ frontend/src/constants/backendEdpoint.js
@@ -3,3 +3,5 @@ export const backendEdpoint = import.meta.env.VITE_BACKEND_URL;
 export const backendProductEnpoint = `${backendEdpoint}/api/products`;
 
 export const backendAuthEnpoint = `${backendEdpoint}/api/users`;
+
+export const backendUserProductEnpoint = `${backendEdpoint}/api/users-products`;
